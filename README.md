59478bf7b76f9032fcb83b2f2c90c14518151b13
Hugo Silva <42209981+hugossilva2@users.noreply.github.com>
2023-08-09 15:16:48 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 treinando programação com Dechamps
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-function Home(){
-
-    return <h1>Débora, Te amo, se vc me ama dê uma risadinha </h1>
-
+function Home() {
+  return <h1>Débora, Te amo, se vc me ama dê uma risadinha </h1>;
 }
 
 export default Home;
